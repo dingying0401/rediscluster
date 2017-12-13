806ebc1cbdfb328a5e11e9d23ec92f54cc7e6b0e
dingying0401 <30583903+dingying0401@users.noreply.github.com>
2017-12-13 10:43:00 +0800

    Update expect.sh

--- expect.sh
+++ expect.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/expect -f
 # set addresslist [lrange $argv 0 5]
+set timeout 40
 spawn ./redis-trib.rb create --replicas 1 $argv                
 expect "Can I set the above configuration? (type 'yes' to accept):"
 send "yes\r"
