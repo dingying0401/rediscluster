e4822b4d8297d936d130deaef4770e6e36cae540
dingying0401 <30583903+dingying0401@users.noreply.github.com>
2017-12-14 11:36:27 +0800

    Update cluster.sh

--- cluster.sh
+++ cluster.sh
@@ -4,13 +4,12 @@
 num=`hostname | cut -d '-' -f 2`
 count=0
 #array for the ip list
-list=()
 addresslist=()
 #catch ip list
 x=`expr $REDIS_NUM - 1`
 #echo $num $x
 if [[ "$num" == "$x" ]];then
-  #echo $num $x
+#echo $num $x
   while [ "$count" -lt "$REDIS_NUM" ]
   do
     if [ "$count" != "$x" ];then
@@ -21,13 +20,12 @@ if [[ "$num" == "$x" ]];then
       IP=`ping $CLUSTERNAME-$count.$SUBDOMAIN -c 1 | grep 10.128 | grep -v 'PING' | cut -d ' ' -f 5 | cut -d '(' -f 2 | cut -d ')' -f 1`
       echo $IP >> bash.log
     fi
-    #list=(${list[*]} $IP)
     address=$IP:6379
     addresslist=(${addresslist[*]} $address)
     count=$[count+1]
   done
   nohup redis-server /etc/redis.conf &
-  #print the ip list
+  #print the ip list into bash.log
   echo ${addresslist[*]} >> bash.log
   #start creating the redis cluster
   #./expect.sh 
@@ -35,6 +33,5 @@ if [[ "$num" == "$x" ]];then
   tail -f /var/log/alternatives.log
 else
   redis-server /etc/redis.conf
- #echo "failed"
 fi
 
